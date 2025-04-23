1a5f93e42b66aabde3d25983ce9ddb466da6591a
Srusti00007 <srustinaik007@gmail.com>
2025-04-23 09:03:27 +0530

    Simple Java Programs

---
+++ pgm3.java
@@ -0,0 +1,15 @@
+package Programs;
+
+public class pgm3 {
+    public static void main(String[] args) {
+        String name="java";
+        if(name=="java")
+        {
+            System.out.println("The Computer Language is "+name);
+        } 
+        else
+        {
+            System.err.println("This Is not Java");
+        }
+    }
+}
